299a076cf4cfea58f8c4fb3e66f08693f2bab78e
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-25 21:10:19 +0000

    Added logic from main

--- Turns.rb
+++ Turns.rb
@@ -1,5 +1,20 @@
-class Games 
-  def self.run_game 
-    puts "Welcome to the game"
+require "./Questions"
+
+class Turns
+  def initialize(current_player)
+    puts "------ New Turn ------"
+    @question = Questions.new
+    @current_player = current_player
+  end
+
+  def run
+    puts "#{@current_player.name}: " + @question.generate_question
+    player_input = gets.chomp
+    if @question.compare_answer(player_input.to_i)
+      puts "That is correct!"
+    else 
+      puts "Sorry, that is incorrect!"
+      @current_player.reduce_lives
+    end
   end
 end
