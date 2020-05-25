991e95febaac71d3a91a69f892cddaf1332a6aea
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-25 21:09:53 +0000

    removed logic and added to Main_Games class

--- main.rb
+++ main.rb
@@ -1,39 +1,5 @@
-require "./Players"
-require "./Questions"
-require "./Games"
+require "./Math_Game"
 
-puts "Welcome to the TwO-O-Player Math Game!"
+newGame = Math_Game.new
 
-player1 = Players.new("Player1")
-player2 = Players.new("Player2")
-questions = Questions.new
-current_player = player1
-
-while (player1.lives > 0 && player2.lives > 0) do 
-  puts "------ New Turn ------"
-
-  puts "#{current_player.name}: " + questions.generate_question
-  player_input = gets.chomp
-  if questions.compare_answer(player_input.to_i)
-    puts "That is correct!"
-  else 
-    puts "Sorry, that is incorrect!"
-    current_player.reduce_lives
-  end
-
-  puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
-  
-  if current_player.name == player1.name
-    current_player = player2
-  else 
-    current_player = player1
-  end
-end
-
-if current_player.name == player2.name 
-  puts "#{player2.name} wins with a score of #{player2.lives}/3!"
-else 
-  puts "#{player1.name} wins with a score of #{player1.lives}/3!"
-end
-puts "------ Game Over ------"
-puts "Good Bye!"
+newGame.run
