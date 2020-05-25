51008c35b47e87b24eaa4960b3a5eca0c728f44b
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-25 21:09:18 +0000

    clean up

--- Players.rb
+++ Players.rb
@@ -1,16 +1,12 @@
 class Players 
-  attr_accessor :lives
+  attr_accessor :lives, :name
   def initialize(name)
     @name = name
     @lives = 3
-    @correctly_answered = 0
   end
 
   def reduce_lives
     @lives -= 1
   end
 
-  def add_correctly_answered 
-    @correctly_answered += 1
-  end
 end
